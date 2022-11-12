71697010f402a981db140af3a6987154619958b7
Dipali-art <67815632+Dipali-art@users.noreply.github.com>
2022-11-12 22:51:33 +0530

    script

---
+++ script.js
@@ -0,0 +1,37 @@
+const quizData = [
+  {
+    question: "Q1: What is HTML?",
+    a: "hyper test markup language",
+    b: "hpyer text markup language",
+    c: "Hey Test Markup Language",
+    d: "Hyper Text Markup Language",
+    ans: "ans4",
+  },
+
+  {
+    question: "Q2: What is long form of CSS?",
+    a: "Cascading Style Sheets",
+    b: "Cascading Styles Sheets",
+    c: "Cascadings Style Sheets",
+    d: "Cascading Style Sheet",
+    ans: "ans1",
+  },
+
+  {
+    question: "Q3:  Choose the correct HTML tag for the largest heading",
+    a: "<heading>",
+    b: "<h1></h1>",
+    c: "<head>",
+    d: "<h6>",
+    ans: "ans4",
+  },
+
+  {
+    question: "Q4: What is the correct HTML tag for inserting a line break?",
+    a: "<br>",
+    b: "<lb>",
+    c: "<break></break>",
+    d: "<break>",
+    ans: "ans1",
+  },
+];
